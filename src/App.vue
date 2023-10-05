e4dc3359f958175edc7f6be96a52a189bee48b86
xiaoli <2585960098@qq.com>
2023-10-05 16:41:23 +0800

    publish1

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>基本模板！</div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  components: {},
+};
+</script>
+
+<style>
+#app{
+  width: 100%;
+  height: 100%;
+}
+</style>
